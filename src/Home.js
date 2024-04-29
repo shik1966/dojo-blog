905309aadb183d715cf8041c04ba272dd24e0c87
shik1966 <marwan.a.shakib@gmail.com>
2024-04-30 01:36:35 +0200

    TUT 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+const handleClick = (e) => {
+    console.log('hello, ninjas');
+} 
+
+const handleClickAgain = (name, e) => {
+    console.log('hello' + name, e.target);
+} 
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={handleClickAgain(' mario')}> Click me again</button>
+            <button onClick = {(e) => handleClickAgain('hello', e)}> Click me one last time</button>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create " /*{style = {{
+                <a href="/create " style = {{
                     color: 'white',
                     backgroundColor: '#f1356d',
                     borderRadius: '8 px' 
-                }}*/> New Blog</a>
+                }}> New Blog</a>
             </div>
         </nav>
      );
