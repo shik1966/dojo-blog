74f0fa0161323ca23736efa0a4a985c42454289d
shik1966 <marwan.a.shakib@gmail.com>
2024-05-01 00:19:47 +0200

    Tut 19

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
 
     const[blogs, setBlogs] = useState(null);
     const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
 
    /* const[name, setName] = useState('Mario');
 
@@ -56,23 +57,32 @@ const Home = () => {
                 setBlogs(newBlogs);
             }*/}
 
-            useEffect(() =>{
-              setTimeout (() => {
-                fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    return res.json();
-                })
-                .then(data  => {
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                })
-              }, 1000);
+            useEffect(() => {
+                setTimeout(() => {
+                    fetch('http://localhost:8000/blogs')
+                        .then(res => {
+                            if (!res.ok) {
+                                throw Error('Could not fetch the data for that resource!');
+                            }
+                            return res.json();
+                        })
+                        .then(data => {
+                            console.log(data);
+                            setBlogs(data);
+                            setIsPending(false);
+                            setError(false);
+                        })
+                        .catch(err => {
+                            setError(err.message);
+                            setIsPending(false);
+                        });
+                }, 1000);
             }, []);
+            
 
             return ( 
                 <div className="home">
-
+                {error && <div>{error}</div>}
                 {isPending && <div> loading...</div>}
                 { blogs && <BlogList blogs = {blogs} title = "All Blogs!" /*handleDelete = {handleDelete}*/ />}
                 {/*<button onClick= {() => setName('SIMO')}> change name </button>
