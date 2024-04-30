9d57705e49b7b612a7b3d23050d4bc8151355efe
shik1966 <marwan.a.shakib@gmail.com>
2024-04-30 03:02:13 +0200

    TUT 8

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
 const Home = () => {
+    
+    //let name = 'mario';
 
-const handleClick = (e) => {
+    /*const handleClick = () => {
+        name = 'luigi';
+        console.log(name);
+    }*/
+
+    const[name, setName] = useState('Mario');
+
+    const[age, setAge] = useState(25);
+
+
+    const handleClick = () => {
+        setName('Luigi');
+        setAge(parseInt(Math.random()*50));
+    } 
+
+    /*const handleClick = (e) => {
     console.log('hello, ninjas');
-} 
+    } */
 
 const handleClickAgain = (name, e) => {
     console.log('hello' + name, e.target);
@@ -11,10 +29,13 @@ const handleClickAgain = (name, e) => {
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={handleClickAgain(' mario')}> Click me again</button>
-            <button onClick = {(e) => handleClickAgain('hello', e)}> Click me one last time</button>
-        </div>
+
+            {/*<{button onClick={handleClickAgain(' mario')}> Click me again</button>}*/}
+            <button onClick = {(e) => handleClickAgain(' marwan', e)}> Click me one last time</button>
+
+        </div>  
     );
 }
  
