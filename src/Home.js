1cfcf7ae053822f49eae8d2ac93306929569bf6d
shik1966 <marwan.a.shakib@gmail.com>
2024-04-30 03:20:51 +0200

    TUT 10

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,13 @@ const Home = () => {
         console.log(name);
     }*/
 
-    const[name, setName] = useState('Mario');
+    const[blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+   /* const[name, setName] = useState('Mario');
 
     const[age, setAge] = useState(25);
 
@@ -22,18 +28,28 @@ const Home = () => {
     console.log('hello, ninjas');
     } */
 
-const handleClickAgain = (name, e) => {
+/*const handleClickAgain = (name, e) => {
     console.log('hello' + name, e.target);
-} 
+}*/ 
 
     return (  
         <div className="home">
-            <h2>Homepage</h2>
+
+            {/*{<h2>Homepage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
 
-            {/*<{button onClick={handleClickAgain(' mario')}> Click me again</button>}*/}
+            {/*<{button onClick={handleClickAgain(' mario')}> Click me again</button>}
             <button onClick = {(e) => handleClickAgain(' marwan', e)}> Click me one last time</button>
+            }*/}
+
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key ={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+
+            ))}
 
         </div>  
     );
